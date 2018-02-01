a0b3caa2a1fd9d0d8cfd8461d209b5bc41c4afeb
harshasaranam <harshasaranam@gmail.com>
2018-01-31 18:18:47 -0600

    p

--- LAB-1/source/4.py
+++ LAB-1/source/4.py
@@ -1,13 +1,12 @@
 pythonlist=['harsha','akshay','harish','avinash','chintu']      #list of students from python class
-weblist=['harsha','chintu','nagraj']                            #list of students from web class
+weblist=['harsha','chintu','nagraj','ramana']                            #list of students from web class
 
 
 def common(a, b):                                #this function helps in finding the ppl who enrolled in both classes
-    return set(a) & set(b)
+    return set(a).intersection(set(b))
 
 def notcommon(a,b):                             #this function helps in finding the ppl who r not common in both classes
-    return (set(a) or set(b))-(set(a) and set(b))
-
+    return (set(a).union(set(b)))-(set(a).intersection(set(b)))
 
 
 print("list of students who are common in both classes:",common(pythonlist, weblist))
